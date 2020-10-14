d3ef6304985d69397cb354252f71671e42b62dc7
farhadmpr <farhadmp@gmail.com>
2020-10-14 11:18:35 +0330

    Add emotion library

---
+++ frontend/src/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomePage = () => (
+  <div>
+    <div>
+      <h2>Unanswerd Questions</h2>
+      <button>Ask a question</button>
+    </div>
+  </div>
+);
